4752aed7168f546e633037ccd4cb6edaed77c4d1
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2019-05-10 11:29:23 -0300

    rrdraco.rrdtool_wrapper.py.rrdtool functions changed to static

--- rrdraco/rrdtool_wrapper.py
+++ rrdraco/rrdtool_wrapper.py
@@ -1,20 +1,21 @@
 from distutils import spawn
 import subprocess
 
+rrdtool_exec = spawn.find_executable('rrdtool')
+
 
 class rrdtool:
-    tool = spawn.find_executable('rrdtool')
 
-    @classmethod
-    def info(cls, rrd_file):
-        cmd = [cls.tool, 'info', rrd_file]
+    @staticmethod
+    def info(rrd_file):
+        cmd = [rrdtool_exec, 'info', rrd_file]
         return subprocess.check_output(cmd, stderr=subprocess.STDOUT).decode()
 
-    @classmethod
-    def create(cls, rrd_file, ds, rra, start="N", step=300):
+    @staticmethod
+    def create(rrd_file, ds, rra, start="N", step=300):
         if not rrd_file.endswith('.rrd'):
             rrd_file += '.rrd'
-        cmd = [cls.tool, 'create', rrd_file,
+        cmd = [rrdtool_exec, 'create', rrd_file,
                '--start', start, '--step', str(step)]
         cmd += ds
         cmd += rra
